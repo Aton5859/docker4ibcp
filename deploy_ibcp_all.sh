e00b6acb8684e7d3008a32f9f4bacaa424b49bc2
牛加人等于朱 <niuren.zhu@icloud.com>
2016-12-13 17:46:38 +0800

    解决软连接TOMCAT不能识别问题。

--- deploy_ibcp_all.sh
+++ deploy_ibcp_all.sh
@@ -97,7 +97,7 @@ while read file
     if [ -e "${IBCP_DEPLOY}/${folder}/config.json" ]; then
       if [ ! -e "${IBCP_CONF}/config.json" ]; then cp -f "${IBCP_DEPLOY}/${folder}/config.json" "${IBCP_CONF}/config.json"; fi;
       rm -f "${IBCP_DEPLOY}/${folder}/config.json"
-      ln -s "${IBCP_CONF}/config.json" "${IBCP_DEPLOY}/${folder}/config.json"
+      ln -P "${IBCP_CONF}/config.json" "${IBCP_DEPLOY}/${folder}/config.json"
     fi;
 # 映射日志文件夹到统一位置
     if [ -e "${IBCP_DEPLOY}/${folder}/WEB-INF/log" ]; then rm -rf "${IBCP_DEPLOY}/${folder}/WEB-INF/log"; fi;
